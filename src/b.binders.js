7a126623e95cdff8e4289e124a4db202d90429d8
kriss <tlaloc042@gmail.com>
2016-05-25 12:41:32 +0200

    updated default binders

--- src/b.binders.js
+++ src/b.binders.js
@@ -32,28 +32,5 @@ b.binders = {
 		}else{
 			el.style.backgroundColor = params[0].object[ params[0].param ]; // set raw value
 		}
-	},
-	
-
-	/// SVG //////////////////////////////////////////////////////////////
-	// set element bgColor
-	fill : function(el,params){
-		
-		if(typeof params.formatted !== "undefined"){
-			el.style.backgroundColor = params.formatted; // set formatted value
-		}else{
-			el.style.fill = params[0].object[ params[0].param ]; // set raw value
-		}
-	},
-	
-	// set element opacity
-	opacity : function(el,params){
-
-		if(typeof params.formatted !== "undefined"){
-	
-			el.style.fillOpacity = params.formatted; // set formatted value
-		}else{
-			el.style.fillOpacity = params[0].object[ params[0].param ]; // set raw value
-		}
 	}
 };
